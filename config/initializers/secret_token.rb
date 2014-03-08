000c24660fdc6f7d70a890b985235ae1f789b59e
Peter Hicks <peter.hicks@poggs.co.uk>
2014-03-08 11:50:23 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CityDashboard::Application.config.secret_key_base = '2d42aed7f6cea1e57b7e418bd2193e54c797b5093dc5810d5950425fc540a262b14ae78aa4ae4ebb6dd7e55c0140f6a72e239629d8d8854510fc105e2f97ca3e'
