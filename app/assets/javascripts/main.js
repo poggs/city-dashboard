efcb1c53445a7876598dbdc5b9aa9affe04db71d
Peter Hicks <peter.hicks@poggs.co.uk>
2014-03-09 10:42:26 +0000

    Add Leaflet.awesome-markers

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,10 +1,16 @@
 function loadStationUsageData() {
 
+  var stationStyle = L.AwesomeMarkers.icon({
+    prefix: 'fa',
+    icon: 'info',
+    markerColour: 'blue'
+  })
+
   $.getJSON('/station_usage.json', function(data) {
     for(var i=0; i<data.length; i++) {
       e = data[i]
       title = e[1];
-      marker = new L.Marker([e[2], e[3]]);
+      marker = new L.Marker([e[2], e[3]], { icon: stationStyle });
 
       var markerText = '';
       markerText = markerText + '<li>2012/13 usage: <em>' + e[13] + '</em></li>'
@@ -31,10 +37,15 @@ function loadWaterAlertData() {
 
 function loadWaterCurrentInvestments() {
 
+  var markerStyle = L.AwesomeMarkers.icon({
+    icon: 'thumbs-up',
+    markerColour: 'green'
+  })
+
   $.getJSON('/yw_investments', function(data) {
     for(var i=0; i<data.length; i++) {
       e = data[i];
-      var marker = new L.Marker([ e.Latitude, e.Longitude ]);
+      var marker = new L.Marker([ e.Latitude, e.Longitude ], { icon: markerStyle });
       var markerText = '<strong>' + e['Name'] + '</strong><br /><em>' + e['Type'] + '</em>';
       markerText = markerText + '<p>' + e['Description'] + '</p>'
       marker.bindPopup(markerText);
